db0d6f54a6d68ee346861da72fc0bfd5049342bc
Pan Ziyue <random.rrr3@gmail.com>
2025-04-01 00:39:27 +0800

    implemented untested seconds to minutes/seconds divider

--- T02/T02_Stopwatch_Project/T02_Stopwatch_Project.srcs/sources_1/new/s_to_mmss.v
+++ T02/T02_Stopwatch_Project/T02_Stopwatch_Project.srcs/sources_1/new/s_to_mmss.v
@@ -19,10 +19,53 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module s_to_mmss(
     input [31:0] s,
-    input [31:0] mm,
-    input [31:0] ss
+    input clk,          // This device relies on the master 100MHz clock to perform simple division
+    output reg [31:0] mm,
+    output reg [31:0] ss
     );
+    
+    // We use simple primary school style division, keep subtracting until we cannot subtract anymore and we get our quotient and remainder :)
+    reg state;
+    reg [31:0]in_buf; // Input buffer. Only takes in s upon state 1
+    reg [31:0]quotient;
+    reg [31:0]remainder;
+    reg [31:0]temp;
+    
+    initial state = 0;
+    initial mm = 0;
+    initial ss = 0;
+    
+    always @(posedge clk) begin
+        if (state == 0) begin
+            // Preserve minutes and seconds registers, do not change their values
+            mm <= mm;
+            ss <= ss;
+            
+            remainder <= temp; // Save the remainder, just in case this subtraction yields a negative number
+            temp <= temp - 60; // Perform division by subtracting 60 repeatedly
+            
+            // if this subtraction operation will result in a negative number, discard the result and transition to state 1
+            if (temp < 0) begin
+                quotient <= quotient;
+                state <= 1;
+            end else begin
+                // If we were able to subtract by 60, save quotient
+                quotient <= temp;
+                state <= state;
+            end
+        end else if (state == 1) begin
+            // Load seconds into the temp register
+            temp <= s;
+            
+            // Load quotient and remainder into minutes and seconds registers
+            mm <= quotient;
+            ss <= remainder;
+            
+            // Transition back to state 0 (calculating state)
+            state <= 0;
+        end
+    end
+    
 endmodule
