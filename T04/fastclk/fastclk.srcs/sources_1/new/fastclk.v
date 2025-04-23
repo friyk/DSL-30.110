8e66a4286942b129a7a4fd6f536e307058767641
Wesley <wesleyngah@gmail.com>
2025-04-23 15:50:36 +0800

    added fastclk module

---
+++ T04/fastclk/fastclk.srcs/sources_1/new/fastclk.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 23.04.2025 14:52:26
+// Design Name: 
+// Module Name: fastclk
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+// using io pin 
+module fastclk(
+    input rst,
+    input sysclk,
+    input pio1,
+    
+    output reg [7:0]counter,
+    output reg [7:0]counterHold
+    );
+
+always @(posedge sysclk or posedge rst) begin
+    if (rst) begin    
+        counter <= 8'b0000000;
+        counterHold <= 8'b00000000;   
+    end
+         
+    else if (~pio1)
+        counter <= counter + 1'b1;
+        
+    else if (pio1)
+        counterHold = counter;
+        counter = 8'b00000000;
+        
+end            
+   
+
+endmodule
